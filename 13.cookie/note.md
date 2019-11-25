7deef362d7ad3bc4a19f56d74d3c6d880fb70d20
Hazelnuttt <1282308134@qq.com>
2019-11-25 18:51:57 +0800

    feat: cookie 签名 && session

--- 13.cookie/note.md
+++ 13.cookie/note.md
@@ -1,9 +1,21 @@
 ## cookie section localStorage sectionStorage 区别
 
-cookie section
+- cookie section
 
-cookie 存于浏览器 4k 不能跨越
-section 基于 cookie 存于服务器 不存在跨域问题
+- cookie 存于浏览器 4k 不能跨越
+- section 基于 cookie 存于服务器 不存在跨域问题
 
-localStorage 存本地 不能跨域 5m
-sectionStorage 浏览器关掉销毁
+- localStorage 存本地 不能跨域 5m
+- sectionStorage 浏览器关掉销毁
+
+- 普通的 cookie 操作很简单
+
+```js
+//设置
+res.setHeader('Set-Cookie', ['name = hazelnut; path=/a', 'age=10'])
+//取
+req.headers['cookie']
+```
+
+- 追加签名
+- 服务端有一把密钥，这把密钥就是根据内容，生成 `.`后面的一串。每次用密钥开内容，看内容是否改变。这把密钥肯定不能是 MD5，可以是`加盐算法`
--- 13.cookie/session.js
+++ 13.cookie/session.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const crypto = require('crypto')
 const secret = 'hazelnut'
-const querystring = require('querystring')
+const cardName = 'hazelnut'
+const uuid = require('uuid')
+let session = {}
 const sign = value => {
   return crypto
     .createHmac('sha256', secret)
@@ -16,17 +18,19 @@ http
     req.getCookie = function(key, opts = {}) {
       let obj = require('querystring').parse(req.headers['cookie'], '; ')
       let cookie = obj[key]
-      let [value, s] = cookie.includes('.') ? cookie.split('.') : [cookie]
-      if (opts.signed) {
-        // 如果有签名要检验
-        if (sign(value) === s) {
-          // 校验cookie的签名，如果没有更改说明值是服务端设置的
-          return value
-        } else {
-          return ''
+      if (cookie) {
+        let [value, s] = cookie.includes('.') ? cookie.split('.') : [cookie]
+        if (opts.signed) {
+          // 如果有签名要检验
+          if (sign(value) === s) {
+            // 校验cookie的签名，如果没有更改说明值是服务端设置的
+            return value
+          } else {
+            return ''
+          }
         }
+        return value
       }
-      return value
     }
 
     let cookies = []
@@ -50,19 +54,21 @@ http
       res.setHeader('Set-Cookie', cookies)
     }
 
-    if (req.url === '/read') {
-      res.end(req.getCookie('name', { signed: true } || '空'))
-      //   res.end(req.headers['cookie'] || '空')
-    } else if (req.url === '/write') {
-      //同域名下共享 domain
-      //   res.setHeader('Set-Cookie', ['name=hazelnut; domain=".zf1.cn"', 'age=19'])
-      //设置path  以这个路径开头
-      //expires 过期 max-age=
-      //httpOnly=true 在服务端设的客户端拿不到
-      //   res.setHeader('Set-Cookie', ['name=hazelnut; path=/a ', 'age=19'])
-      res.setCookie('name', 'hazelnut', { signed: true })
-      res.setCookie('age', '19')
-      res.end('write end')
+    if (req.url === '/visit') {
+      let CardId = req.getCookie(cardName, { signed: true })
+      if (CardId && session[CardId]) {
+        session[CardId].visit++
+        res.setHeader('Content-Type', 'text/html;charset=utf-8')
+        res.end(`这是第${session[CardId].visit}次来`)
+      } else {
+        let CardId = uuid.v4()
+        res.setCookie(cardName, CardId, { signed: true })
+        session[CardId] = {
+          visit: 1
+        }
+        res.setHeader('Content-Type', 'text/html;charset=utf-8')
+        res.end(`这是第一次来`)
+      }
     }
   })
   .listen(3000)
